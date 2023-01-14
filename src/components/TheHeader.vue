1b74e892eed22854a8f8d12cdecc229504e74ebd
Iskander <kubeev2001@gmail.com>
2023-01-14 16:05:47 +0600

    header added

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__wrapper">
+        <a href="/" class="header__link">Where is the world?</a>
+        <div class="theme-btn">
+          <img src="../assets/moon.svg" alt="">
+          <span>Dark Mode</span>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
